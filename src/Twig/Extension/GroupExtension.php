917d5b6e770ef72116475bc6013151b66339f426
manuel_j555 <programador.manuel@gmail.com>
2014-01-08 21:11:55 -0430

    Se cambia el namespace del autoload del composer y se agrega la clase

---
+++ src/Twig/Extension/GroupExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace K2\Group\Twig\Extension;
+
+use Twig_Extension;
+use Twig_SimpleFilter;
+
+/**
+ * Description of AssetsExtension
+ *
+ * @author Manuel Aguirre <programador.manuel@gmail.com>
+ */
+class GroupExtension extends Twig_Extension
+{
+
+    public function getName()
+    {
+        return 'group';
+    }
+
+    public function getFilters()
+    {
+        return array(
+            new Twig_SimpleFilter('group', array($this, 'group')),
+        );
+    }
+
+    public function group($items)
+    {
+        
+    }
+
+}
