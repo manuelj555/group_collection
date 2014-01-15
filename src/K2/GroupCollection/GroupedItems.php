8b4bd8bb2b8515d3f83ad2e5212665e4448c1540
manuel_j555 <programador.manuel@gmail.com>
2014-01-15 16:12:25 -0430

    commit

--- src/K2/GroupCollection/GroupedItems.php
+++ src/K2/GroupCollection/GroupedItems.php
@@ -2,12 +2,19 @@
 
 namespace K2\GroupCollection;
 
+use ArrayAccess;
+use ArrayIterator;
+use BadMethodCallException;
+use InvalidArgumentException;
+use IteratorAggregate;
+use LogicException;
+
 /**
  * Description of GrupedItems
  *
  * @author Manuel Aguirre <programador.manuel@gmail.com>
  */
-class GroupedItems implements \IteratorAggregate
+class GroupedItems implements IteratorAggregate, ArrayAccess
 {
 
     protected $title;
@@ -26,7 +33,7 @@ class GroupedItems implements \IteratorAggregate
 
     public function getIterator()
     {
-        return new \ArrayIterator($this->items);
+        return new ArrayIterator($this->items);
     }
 
     public function add($item)
@@ -37,6 +44,44 @@ class GroupedItems implements \IteratorAggregate
     public function __toString()
     {
         return (string) $this->getTitle();
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (!method_exists($this->getTitle(), $name)) {
+            throw new BadMethodCallException("Method $name does not exist");
+        }
+
+        return call_user_func_array(array($this->getTitle(), $name), $arguments);
+    }
+
+    public function __get($name)
+    {
+        if (!property_exists($this->getTitle(), $name)) {
+            throw new InvalidArgumentException("property $name does not exist");
+        }
+
+        return $this->getTitle()->{$name};
+    }
+
+    public function offsetExists($offset)
+    {
+        return is_array($this->title) && isset($this->title[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->offsetExists($offset) ? $this->title[$offset] : null;
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        throw new LogicException('Not supported');
+    }
+
+    public function offsetUnset($offset)
+    {
+        throw new LogicException('Not supported');
     }
 
 }
